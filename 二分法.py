d7364145def222ed093f895e832986230dfb5b69
Gyhhh28 <100687327+Gyhhh28@users.noreply.github.com>
2023-02-21 23:57:38 +0800

    Update 二分法.py

--- 二分法.py
+++ 二分法.py
@@ -1,7 +1,7 @@
 def binary_search(lst, target):
     lef = 0
-    rig = len(lst)-1
-    while lef <= rig:
+    rig = len(lst)-1 #如果是len（lst） 这个值是大于列表序列的最大值的 因为从0开始 所以取不到 开区间
+    while lef <= rig: #如果在len不减一的情况下还写等于，那就是取一个不在列表里的值 怎么可能 溢出了
         mid = (lef+rig)//2
         if lst[mid] == target:
             return mid
